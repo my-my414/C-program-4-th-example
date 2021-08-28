917f120bcba64fb1b788b538bc46631408e0497b
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-08-28 18:22:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+//write a program to print the following output (there are two blank lines in the middle)
+#include<stdio.h>
+int main()
+{
+    printf("Name\t:\tMr.DOn\n");
+    printf("Country\t:\tBangladesh\n\n\n");
+    printf("contact\t:\tmr.don@mycompany.com\n");
+    return 0;
+}
